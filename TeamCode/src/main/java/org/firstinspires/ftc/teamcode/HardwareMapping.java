2a868383784fcae83b15a3c47ea0ba801a61d3b7
Guy Reams <guyreams@gmail.com>
2019-11-01 16:08:52 -0700

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapping.java
@@ -1,12 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class HardwareMapping {
 
-    public CRServo ArmLiftServo;
+    public CRServo SlideServo;
+    public CRServo ArmServo;
+    public Servo ClawServo;
+    public Servo WristServo;
+    public Servo LazyServo;
+    public Servo HitchServo;
+    public DcMotor leftFrontDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightBackDrive = null;
+
 
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
@@ -18,7 +30,16 @@ public class HardwareMapping {
 
         hwMap = ahwMap;
 
-        ArmLiftServo = hwMap.get(CRServo.class, "ArmLift");
+        SlideServo = hwMap.get(CRServo.class, "Slide");
+        ArmServo = hwMap.get(CRServo.class, "Arm");
+        ClawServo = hwMap.servo.get("Claw");
+        WristServo = hwMap.servo.get("Wrist");
+        LazyServo = hwMap.servo.get("Lazy");
+        HitchServo = hwMap.servo.get("Hitch");
+        leftFrontDrive = hwMap.get(DcMotor.class,"front_left");
+        rightFrontDrive = hwMap.get(DcMotor.class,"front_right");
+        leftBackDrive = hwMap.get(DcMotor.class,"back_left");
+        rightBackDrive = hwMap.get(DcMotor.class,"back_right");
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -2,40 +2,149 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOp Mode")
 public class TeleOpMode extends OpMode {
 
     HardwareMapping robot = new HardwareMapping();
 
+    // public final static double ARM_HOME = 0.5;
+    // public final static double ARM_MIN = 0.0;
+    // public final static double ARM_MAX = 1.0;
+    // public final static double SLIDE_HOME = 0.5;
+    // public final static double SLIDE_MIN = 0.0;
+    // public final static double SLIDE_MAX = 1.0;
+    public final static double REV_HOME = 0.5;
+    public final static double REV_MIN = 0.0;
+    public final static double REV_MAX = 1.0;
+    // public final double ARM_SPEED = 0.01;
+    // public double armPosition = ARM_HOME;
+
+
     public void init ()
+
     {
         robot.init(hardwareMap);
+        robot.ClawServo.setPosition(REV_HOME);
+        robot.WristServo.setPosition(REV_HOME);
+        robot.LazyServo.setPosition(REV_HOME);
+        robot.HitchServo.setPosition(REV_HOME);
+        robot.leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        robot.rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        robot.leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        robot.rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
     }
 
     public void loop ()
+
     {
+        double leftFrontPower;
+        double rightFrontPower;
+        double leftBackPower;
+        double rightBackPower;
+        double drive = gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+        double strafe = gamepad1.left_stick_x;
+
+
+        leftFrontPower = Range.clip(drive-strafe+turn,-1.0,1.0);
+        leftBackPower = Range.clip(drive+strafe+turn,-1.0,1.0);
+        rightFrontPower = Range.clip(drive+strafe-turn,-1.0,1.0);
+        rightBackPower = Range.clip(drive-strafe-turn,-1.0,1.0);
+
+        /*
+        Arm - dpad.up, dpad.down
+        Lazy - dpad.right, dpad.left
+        Hitch    - left_trigger, right_trigger
+        Slide - left_bumper, right_bumper
+        Claw - x, b
+        Wrist - y, a
+         */
+
+        if (gamepad1.x)
+        {
+            robot.ClawServo.setPosition(REV_MAX);
+        }
+
+        if (gamepad1.b)
+        {
+            robot.ClawServo.setPosition(REV_MIN);
+        }
+
+        if (gamepad1.y)
+        {
+            robot.WristServo.setPosition(REV_MAX);
+        }
 
         if (gamepad1.a)
         {
-            robot.ArmLiftServo.setPower(.75);
+            robot.WristServo.setPosition(REV_MIN);
         }
 
-        else if (gamepad1.y)
+        if (gamepad1.dpad_right)
         {
-            robot.ArmLiftServo.setPower(-.75);
+            robot.LazyServo.setPosition(REV_MAX);
+        }
+
+        if (gamepad1.dpad_left)
+        {
+            robot.LazyServo.setPosition(REV_MIN);
+        }
+
+        if (gamepad1.right_trigger > 0.5)
+        {
+            robot.HitchServo.setPosition(REV_MAX);
+        }
+
+        if (gamepad1.left_trigger > 0.5)
+        {
+            robot.HitchServo.setPosition(REV_MIN);
+        }
+
+        if (gamepad1.dpad_up)
+        {
+            robot.ArmServo.setPower(.75);
+        }
+
+        else if (gamepad1.dpad_down)
+        {
+            robot.ArmServo.setPower(-.75);
         }
 
         else
         {
-            robot.ArmLiftServo.setPower(0);
+            robot.ArmServo.setPower(0);
         }
 
+        if (gamepad1.left_bumper)
+        {
+            robot.SlideServo.setPower(.75);
+        }
+
+        else if (gamepad1.right_bumper)
+        {
+            robot.SlideServo.setPower(-.75);
+        }
+
+        else
+        {
+            robot.SlideServo.setPower(0);
+        }
+
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
+        telemetry.update();
     }
 
     public void stop ()
     {
-        robot.ArmLiftServo.setPower(0);
+        robot.SlideServo.setPower(0);
+        robot.ArmServo.setPower(0);
+        robot.ClawServo.setPosition(REV_HOME);
+        robot.WristServo.setPosition(REV_HOME);
+        robot.LazyServo.setPosition(REV_HOME);
+        robot.HitchServo.setPosition(REV_HOME);
     }
 
 
