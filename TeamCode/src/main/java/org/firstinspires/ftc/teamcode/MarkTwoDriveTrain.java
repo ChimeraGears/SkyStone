f434a3b1c9fa85a77f3946e38ee15f8bd9ab0f39
MININT-8EE28Q7\greams <guyreams@gmail.com>
2019-11-06 19:12:02 -0800

    JB: Functioning Drive Train (Can Strafe!)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarkTwoDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarkTwoDriveTrain.java
@@ -6,12 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Mark Two TeleOp")
+//12.1211 inches per rotation--JB
 public class MarkTwoDriveTrain extends OpMode{
     HardwareMapping robot = new HardwareMapping();
     SleepFunction sleep = new SleepFunction();
-
-    public final static double REV_MIN = 0.07;
-    public final static double REV_MAX = 1.0;
 
     public void init() {
         robot.init(hardwareMap);
@@ -29,10 +27,10 @@ public class MarkTwoDriveTrain extends OpMode{
         double turn = gamepad1.right_stick_x;
         double strafe = gamepad1.left_stick_x;
 
-        leftFrontPower = Range.clip(drive-strafe+turn,-1.0,1.0);
-        leftBackPower = Range.clip(drive+strafe+turn,-1.0,1.0);
-        rightFrontPower = Range.clip(drive+strafe-turn,-1.0,1.0);
-        rightBackPower = Range.clip(drive-strafe-turn,-1.0,1.0);
+        leftFrontPower = Range.clip(drive+strafe+turn,-1.0,1.0);
+        leftBackPower = Range.clip(drive-strafe+turn,-1.0,1.0);
+        rightFrontPower = Range.clip(drive-strafe-turn,-1.0,1.0);
+        rightBackPower = Range.clip(drive+strafe-turn,-1.0,1.0);
 
     telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
     telemetry.update();
